a32fbba8ef2da88846601e0ef174f3f237574657
vitto <vittoriobelardo@yahoo.it>
2024-03-26 21:04:05 +0100

    Fixed height of the window

--- TrackRecordedPage.py
+++ TrackRecordedPage.py
@@ -11,13 +11,15 @@ class ScrollablePlotFrame(tk.Frame):
 
         self.canvas = tk.Canvas(self)
         self.scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
-        self.scrollable_frame = tk.Frame(self.canvas, bg="orange")
+        self.scrollable_frame = tk.Frame(self.canvas)
         self.scrollable_frame.bind("<Configure>", self.on_frame_configure)
 
-        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+        id = self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
-        self.canvas.pack(side="left", fill="both", expand=True)
+        # self.canvas.bind('<Configure>', lambda e: self.canvas.itemconfigure(id, width=e.width))
+        self.canvas.pack(side="left", fill=tk.BOTH, expand=True)
         self.scrollbar.pack(side="right", fill="y")
+
 
     def on_frame_configure(self, event):
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
